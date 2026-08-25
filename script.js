895e69b6611069f0c9b7eb08a7bc588353da1f16
Shubh_Maheta <shubhmaheta383@gmail.com>
2026-08-25 14:04:15 +0530

    This simple but cool expenses tracker

---
+++ script.js
@@ -0,0 +1,11 @@
+const Total_Balance = document.getElementById("Total_Balance");
+const Total_Income = document.getElementById('Total_Income');
+const Total_expenses = document.getElementById('Total_expenses');
+const Total_amount = document.getElementById("Total_amount");
+const Full_description = document.getElementById("Full_description");
+
+let transaction_list = document.getElementsByClassName("transaction_list")
+
+function add_transaction(){
+    if()
+}
