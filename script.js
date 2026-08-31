d4e69c8a8b3c5834bde79271c8ffe26bb6fd7cf6
Shubh_Maheta <shubhmaheta383@gmail.com>
2026-08-31 18:21:01 +0530

    Add more features

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const Total_Income = document.getElementById('Total_Income');
 const Total_expenses = document.getElementById('Total_expenses');
 const Total_amount = document.getElementById("Total_amount");
 const Full_description = document.getElementById("Full_description");
+const btn = document.getElementsByClassName('add');
+const transaction_list = document.getElementsByClassName("transaction_list")
+// let transaction_lists = JSON.parse( localStorage.getItem("transaction_lists"))
 
-let transaction_list = document.getElementsByClassName("transaction_list")
-
-function add_transaction(){
-    if()
-}
+btn[0].addEventListener('submit', function(e) {
+    e.preventDefault();
+    const  description = Full_description.value().trim();
+    const amount = parseFloat(Total_amount.value());
+    console.log("description", description);
+    console.log("amount", amount);
+    //get values from local storage
+    let transaction_lists = JSON.parse(localStorage.getItem("transaction_lists")) || [];
+    
+});
